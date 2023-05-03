2c825cdda91f3b6ff0dae781e46eed6d72a93ba5
B_Nurmagambetov <b_nurmagambetov@kbtu.kz>
2023-05-03 19:21:14 +0600

    Bakah

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
-import (
-	"e-commerce-final/users"
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
-	"log"
-	"net/http"
-)
-
-func main() {
-	router := gin.Default()
-	userController := users.NewUserController()
-
-	router.POST("/users", userController.CreateUser)
-	router.POST("/users/token", userController.LogIn)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
-	//t, b, _ := users.TokenGenerator("bidaybek044@gmail.com", "Danial", "Bidaibek", "1234")
-	//bakha chort
-	//hjkl;
-}
+//
+//import (
+//	"e-commerce-final/users"
+//	"github.com/gin-gonic/gin"
+//	_ "github.com/lib/pq"
+//	"log"
+//	"net/http"
+//)
+//
+//func main() {
+//	router := gin.Default()
+//	userController := users.NewUserController()
+//
+//	router.POST("/users", userController.CreateUser)
+//	router.POST("/users/token", userController.LogIn)
+//
+//	log.Fatal(http.ListenAndServe(":8080", router))
+//	//t, b, _ := users.TokenGenerator("bidaybek044@gmail.com", "Danial", "Bidaibek", "1234")
+//	//bakha chort
+//	//hjkl;
+//}
--- products/models.go
+++ products/models.go
@@ -1,23 +1,19 @@
 package products
 
 import (
-	"e-commerce-final/users"
 	"gorm.io/gorm"
 )
 
 type Product struct {
 	gorm.Model
-	User     users.User `gorm:"foreignKey:user"`
-	Category Category   `gorm:"foreignKey:category"`
-	//CategoryID uint
 	Title string `gorm:"size: 150;unique"`
 
 	Body       string `json:"body"`
 	MainImage  string `json:"main_image"`
 	IsActive   bool   `json:"is_active"`
 	IsTop      bool   `json:"is_top"`
-	UserID     uint   `gorm:"column:user_id"`
-	CategoryID uint   `gorm:"column:category_id"`
+	UserID     uint   `json:"user_id" binding:"required" gorm:"ForeignKey:User.ID"`
+	CategoryID uint   `json:"category_id" binding:"required" gorm:"ForeignKey:Category.ID"`
 }
 
 type Category struct {
